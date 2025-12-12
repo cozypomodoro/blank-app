80145cce53e947bb4653f0961af67bbd945e0c9a
cozypomodoro <143543001+cozypomodoro@users.noreply.github.com>
2025-12-12 18:43:53 -0500

    Delete utils directory

--- utils/state.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def init_session():
-    if "workflow" not in st.session_state:
-        st.session_state["workflow"] = []
-    if "df" not in st.session_state:
-        st.session_state["df"] = None
